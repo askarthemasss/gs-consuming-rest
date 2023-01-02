44772efe167ee74c712f31ae88f7caf298efc40a
askar <mohamedaskar476@gmail.com>
2023-01-02 07:53:38 +0530

    Initialized Maven project for Consuming REST service

---
+++ src/main/java/com/askar/consumingrest/Quote.java
@@ -0,0 +1,14 @@
+package com.askar.consumingrest;
+
+
+/*
+ * A more useful way to consume a REST web service is programmatically.
+ * To help you with that task, Spring provides a convenient template class called RestTemplate.
+ * RestTemplate makes interacting with most RESTful services a one-line incantation.
+ * And it can even bind that data to custom domain types.
+ * 
+ */
+
+public class Quote {
+
+}
